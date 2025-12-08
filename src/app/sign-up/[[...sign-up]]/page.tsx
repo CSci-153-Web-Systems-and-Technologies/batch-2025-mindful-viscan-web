496ec8cc58918f67adc0ae738bd93ffe38724a65
patatas <23-1-01732@vsu.edu.ph>
2025-12-08 12:55:56 +0800

    feat: add counselor application link to sign-up page

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,4 +1,5 @@
 import { SignUp } from '@clerk/nextjs';
+import Link from 'next/link';
 import NavBar from '@/app/components/NavBar';
 
 export default function SignUpPage() {
@@ -28,6 +29,19 @@ export default function SignUpPage() {
                 },
               }}
             />
+          </div>
+          
+          {/* Counselor Section */}
+          <div className="mt-8 text-center space-y-4">
+            <p className="text-gray-200 font-kodchasan text-lg">
+              Are you a certified professional?
+            </p>
+            <Link 
+              href="/sign-up-counselor"
+              className="inline-block px-6 py-3 rounded-lg bg-[#0F1E0F] border border-[#42734D]/30 text-[#42734D] font-medium hover:bg-[#42734D] hover:text-white transition-colors duration-200"
+            >
+              Apply as a Counselor
+            </Link>
           </div>
         </div>
       </div>
